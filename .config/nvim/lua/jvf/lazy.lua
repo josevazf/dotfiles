0e15573a56a52e580ab3060b7f28e2646ed7f6c5
josevazf <josevaz.fco@gmail.com>
2024-08-17 10:21:41 +0100

    Lsp config

--- .config/nvim/lua/jvf/lazy.lua
+++ .config/nvim/lua/jvf/lazy.lua
@@ -12,7 +12,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("jvf.plugins", {
+require("lazy").setup({ { import = "jvf.plugins" }, { import = "jvf.plugins.lsp" } }, {
     checker = {
         enabled = true,
         notify = false,
