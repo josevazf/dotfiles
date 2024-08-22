11616e146e8a3e72417762e351389d26e9d5602c
josevazf <josevaz.fco@gmail.com>
2024-08-22 11:53:15 +0100

    Updated lualine

--- .config/nvim/lua/jvf/plugins/lualine.lua
+++ .config/nvim/lua/jvf/plugins/lualine.lua
@@ -8,34 +8,21 @@ return {
         lualine.setup({
             options = {
                 theme = "OceanicNext",
-                component_separators = "|",
-                section_separators = { left = "", right = "" },
+                component_separators = { left = "", right = "" },
+                section_separators = { left = "", right = "" },
+                -- component_separators = "",
+                -- section_separators = { left = "", right = "" },
             },
             sections = {
-                sections = {
-                    lualine_a = { { "mode", separator = { left = "" }, right_padding = 10 } },
-                    lualine_b = { "filename", "branch" },
-                    lualine_c = {
-                        "datetime",
-                        style = "default",
-                    },
-                    lualine_x = {
+                lualine_x = {
+                    {
                         lazy_status.updates,
                         cond = lazy_status.has_updates,
                         color = { fg = "#ff9e64" },
                     },
-                    lualine_y = { "filetype", "progress" },
-                    lualine_z = {
-                        { "location", separator = { right = "" } },
-                    },
-                },
-                inactive_sections = {
-                    lualine_a = { "filename" },
-                    lualine_b = {},
-                    lualine_c = {},
-                    lualine_x = {},
-                    lualine_y = {},
-                    lualine_z = { "location" },
+                    { "encoding" },
+                    { "fileformat" },
+                    { "filetype", icon_only = true },
                 },
             },
         })
