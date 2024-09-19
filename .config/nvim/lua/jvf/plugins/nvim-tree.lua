6e14e3f534fcfee6e7867cd7f2349ab4f1b80ba6
josevazf <josevaz.fco@gmail.com>
2024-09-19 10:14:03 +0100

    Updated nvim-tree mappings

--- .config/nvim/lua/jvf/plugins/nvim-tree.lua
+++ .config/nvim/lua/jvf/plugins/nvim-tree.lua
@@ -58,14 +58,16 @@ return {
         -- set keymaps
         local keymap = vim.keymap -- for conciseness
 
-        keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+        keymap.set("n", "<leader>ew", "<cmd>NvimTreeFocus<CR>", { desc = "Focus file explorer" })
+        keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+        keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFile<CR>", { desc = "Open file explorer on current file" })
+        keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
         keymap.set(
             "n",
-            "<leader>ef",
-            "<cmd>NvimTreeFindFileToggle<CR>",
-            { desc = "Toggle file explorer on current file" }
-        ) -- toggle file explorer on current file
-        keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-        keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+            "<leader>ek",
+            "<cmd>NvimTreeCollapseKeepBuffer<CR>",
+            { desc = "Collapse but keep used directories open" }
+        )
+        keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
     end,
 }
